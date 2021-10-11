10e6afbc3530d40309edf71a1c0433d3a8a5a35b
Mert Çakar <mertcakar.es@gmail.com>
2021-10-12 00:43:00 +0300

    Detailed test is added for Contact List.

--- src/components/contact-example/contact-main.test.tsx
+++ src/components/contact-example/contact-main.test.tsx
@@ -1,5 +1,6 @@
 import "@testing-library/jest-dom/extend-expect";
 import { render, screen } from "@testing-library/react";
+import UserEvent from "@testing-library/user-event";
 import Contacts from "./index";
 
 describe("Contact Main", () => {
@@ -8,9 +9,44 @@ describe("Contact Main", () => {
 	});
 
 	test("Contact List title", () => {
-		var contactsElement = screen.queryByText("Contact List");
+		const contactsElement = screen.queryByText("Contact List");
 
 		expect(contactsElement).toBeInTheDocument();
 		expect(contactsElement).toBeVisible();
+	});
+
+	test("Contact List add contact", () => {
+		const name = "chivalrous";
+		const phone = "0 555 444 33 22";
+
+		const inputName = screen.queryByPlaceholderText("Cantact Name");
+		inputName && UserEvent.type(inputName, name);
+
+		const inputPhone = screen.queryByPlaceholderText("Phone Number");
+		inputPhone && UserEvent.type(inputPhone, phone);
+
+		const addButton = screen.queryByText("Add");
+		addButton && UserEvent.click(addButton);
+
+		const nameDisplayElement = screen.queryByText(name);
+		const phoneDisplayElement = screen.queryByText(phone);
+
+		expect(inputName).toBeInTheDocument();
+		expect(inputName).toBeVisible();
+
+		expect(inputPhone).toBeInTheDocument();
+		expect(inputPhone).toBeVisible();
+
+		expect(addButton).toBeInTheDocument();
+		expect(addButton).toBeVisible();
+
+		expect(nameDisplayElement).toBeInTheDocument();
+		expect(nameDisplayElement).toBeVisible();
+
+		expect(phoneDisplayElement).toBeInTheDocument();
+		expect(phoneDisplayElement).toBeVisible();
+
+		expect(inputName?.innerHTML).toEqual("");
+		expect(inputPhone?.innerHTML).toEqual("");
 	});
 });
