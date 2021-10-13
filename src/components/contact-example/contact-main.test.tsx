24181cc44a73885c847768517c55ca5ff3d8b2e3
Mert Çakar <mertcakar.es@gmail.com>
2021-10-13 23:30:51 +0300

    Filter test is added for Contact Page.

--- src/components/contact-example/contact-main.test.tsx
+++ src/components/contact-example/contact-main.test.tsx
@@ -16,11 +16,13 @@ describe("Contact Main", () => {
 	});
 
 	test("Contact List add contact", () => {
-		const name = "chivalrous";
+		const nameStep1 = "chiva";
+		const nameStep2 = "lrous";
 		const phone = "0 555 444 33 22";
 
 		const inputName = screen.queryByPlaceholderText("Cantact Name");
-		inputName && UserEvent.type(inputName, name);
+		inputName && UserEvent.type(inputName, nameStep1);
+		inputName && UserEvent.type(inputName, nameStep2);
 
 		const inputPhone = screen.queryByPlaceholderText("Phone Number");
 		inputPhone && UserEvent.type(inputPhone, phone);
@@ -28,7 +30,47 @@ describe("Contact Main", () => {
 		const addButton = screen.queryByText("Add");
 		addButton && UserEvent.click(addButton);
 
-		const nameDisplayElement = screen.queryByText(name);
+		const nameDisplayElement = screen.queryByText(nameStep1 + nameStep2);
+		const phoneDisplayElement = screen.queryByText(phone);
+
+		expect(inputName).toBeInTheDocument();
+		expect(inputName).toBeVisible();
+
+		expect(inputPhone).toBeInTheDocument();
+		expect(inputPhone).toBeVisible();
+
+		expect(addButton).toBeInTheDocument();
+		expect(addButton).toBeVisible();
+
+		expect(nameDisplayElement).toBeInTheDocument();
+		expect(nameDisplayElement).toBeVisible();
+
+		expect(phoneDisplayElement).toBeInTheDocument();
+		expect(phoneDisplayElement).toBeVisible();
+
+		expect(inputName?.innerHTML).toEqual("");
+		expect(inputPhone?.innerHTML).toEqual("");
+	});
+
+	test("Contact List filter", () => {
+		const nameStep1 = "chiva";
+		const nameStep2 = "lrous";
+		const phone = "0 555 444 33 22";
+
+		const inputName = screen.queryByPlaceholderText("Cantact Name");
+		inputName && UserEvent.type(inputName, nameStep1);
+		inputName && UserEvent.type(inputName, nameStep2);
+
+		const inputPhone = screen.queryByPlaceholderText("Phone Number");
+		inputPhone && UserEvent.type(inputPhone, phone);
+
+		const addButton = screen.queryByText("Add");
+		addButton && UserEvent.click(addButton);
+
+		const filterInput = screen.queryByPlaceholderText("Cantact Filter");
+		filterInput && UserEvent.type(filterInput, nameStep1);
+
+		const nameDisplayElement = screen.queryByText(nameStep1 + nameStep2);
 		const phoneDisplayElement = screen.queryByText(phone);
 
 		expect(inputName).toBeInTheDocument();
