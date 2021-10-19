595670a3c73859d3ce360729df4643c56c14acd5
Mert Çakar <mertcakar.es@gmail.com>
2021-10-19 23:19:54 +0300

    Increase and decrease process is added with redux.

--- src/components/redux-example/CounterRedux.tsx
+++ src/components/redux-example/CounterRedux.tsx
@@ -1,12 +1,40 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import {
+	customIncrement,
+	decrement,
+	increment,
+} from "../../redux/counter/counterSlice";
 
 const CounterRedux = () => {
 	const counterValue = useSelector((state: any) => state.counter.value);
-
+	const dispatch = useDispatch();
 	return (
 		<div>
 			<h1>{counterValue}</h1>
+
+			<hr />
+			<button
+				onClick={() => {
+					dispatch(decrement());
+				}}
+			>
+				Decreasing
+			</button>
+			<button
+				onClick={() => {
+					dispatch(increment());
+				}}
+			>
+				Increasing
+			</button>
+			<button
+				onClick={() => {
+					dispatch(customIncrement(2));
+				}}
+			>
+				Increasing (2)
+			</button>
 		</div>
 	);
 };
--- src/redux/counter/counterSlice.tsx
+++ src/redux/counter/counterSlice.tsx
@@ -5,7 +5,18 @@ export const counterSlice = createSlice({
 	initialState: {
 		value: 0,
 	},
-	reducers: {},
+	reducers: {
+		increment: (state) => {
+			state.value += 1;
+		},
+		decrement: (state) => {
+			state.value -= 1;
+		},
+		customIncrement: (state, action) => {
+			state.value += action.payload;
+		},
+	},
 });
 
+export const { increment, decrement, customIncrement } = counterSlice.actions;
 export default counterSlice.reducer;
